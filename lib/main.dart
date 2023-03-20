f9e32e57460d224999b42680542a0156818f6526
Yura <yuraberezhnyk@gmail.com>
2023-03-20 17:02:45 +0200

    Created prototpe of Main Page

--- lib/main.dart
+++ lib/main.dart
@@ -39,14 +39,23 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   @override
+  bool favouriteState = false;
+  var urlList = [
+    'https://ggsc.s3.amazonaws.com/images/uploads/The_Science-Backed_Benefits_of_Being_a_Dog_Owner.jpg',
+    'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/Cat03.jpg/1200px-Cat03.jpg',
+    'https://tabimg.pravda.com/images/doc/1/9/1956261-gettyimages-1243769507.jpg',
+    'https://kor.ill.in.ua/m/610x385/2212556.jpg',
+    'https://static.tildacdn.com/tild6661-3962-4165-b862-393466656435/picasso_muza.jpg',
+    'https://d1rs3px2gf3dlr.cloudfront.net/media/blog/featured/PtVBand.jpg'
+  ];
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        toolbarHeight: 65,
+        toolbarHeight: 60,
         backgroundColor: Colors.white,
         title: Text(
           widget.title,
-          style: const TextStyle(color: Colors.black),
+          style: const TextStyle(color: Colors.black, fontSize: 20),
         ),
         actions: [
           IconButton(
@@ -64,19 +73,51 @@ class _MyHomePageState extends State<MyHomePage> {
       body: Column(
         children: [
           SizedBox(
-              height: 110,
-              child: Card(
-                child: ListView.builder(
-                  scrollDirection: Axis.horizontal,
-                  itemCount: 10,
-                  itemBuilder: (BuildContext context, int index) {
-                    return Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.center,
+            height: 110,
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemCount: 10,
+              itemBuilder: (BuildContext context, int index) {
+                return Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Container(
+                      width: 60,
+                      height: 60,
+                      margin: const EdgeInsets.all(10),
+                      decoration: BoxDecoration(
+                        color: Colors.blueGrey,
+                        borderRadius: BorderRadius.circular(30),
+                      ),
+                      child: const Center(),
+                    ),
+                    Text(
+                      "person $index",
+                      style: const TextStyle(fontSize: 12),
+                    )
+                  ],
+                );
+              },
+            ),
+          ),
+          Expanded(
+            child: ListView.builder(
+              scrollDirection: Axis.vertical,
+              itemCount: urlList.length,
+              itemBuilder: (BuildContext context, int index) {
+                return Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Container(
+                      height: 10,
+                    ),
+                    Row(
                       children: [
                         Container(
-                          width: 60,
-                          height: 60,
+                          width: 30,
+                          height: 30,
                           margin: const EdgeInsets.all(10),
                           decoration: BoxDecoration(
                             color: Colors.blueGrey,
@@ -84,15 +125,107 @@ class _MyHomePageState extends State<MyHomePage> {
                           ),
                           child: const Center(),
                         ),
-                        Text(
-                          "person $index",
-                          style: const TextStyle(fontSize: 12),
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: const [
+                            Text(
+                              'Name',
+                              style: TextStyle(fontWeight: FontWeight.bold),
+                            ),
+                            Text('Position')
+                          ],
+                        ),
+                        Expanded(child: Container()),
+                        IconButton(
+                            onPressed: () {}, icon: const Icon(Icons.more_vert))
+                      ],
+                    ),
+                    Container(
+                        width: 400,
+                        height: 400,
+                        margin: const EdgeInsets.all(10),
+                        decoration: const BoxDecoration(
+                          color: Colors.blueGrey,
+                        ),
+                        child: Image.network(
+                          urlList[index],
+                          fit: BoxFit.fill,
+                        )),
+                    Container(height: 5),
+                    Row(
+                      children: [
+                        IconButton(
+                            onPressed: () {
+                              ChangeFavouriteState();
+                            },
+                            icon: FavouriteIcon()),
+                        IconButton(
+                            onPressed: () {},
+                            icon: const Icon(Icons.comment_rounded, size: 25)),
+                        IconButton(
+                            onPressed: () {},
+                            icon: const Icon(Icons.send_sharp, size: 25)),
+                        Expanded(child: Container()),
+                        IconButton(
+                            onPressed: () {},
+                            icon: const Icon(
+                              Icons.bookmark_border,
+                              size: 25,
+                            )),
+                      ],
+                    ),
+                    Container(height: 5),
+                    Row(
+                      children: [
+                        Transform.translate(
+                          offset: const Offset(0, 0),
+                          child: Container(
+                            width: 20,
+                            height: 20,
+                            margin: const EdgeInsets.only(left: 10),
+                            decoration: BoxDecoration(
+                              color: Colors.red,
+                              borderRadius: BorderRadius.circular(20),
+                            ),
+                            child: const Center(),
+                          ),
+                        ),
+                        Transform.translate(
+                          offset: const Offset(-10, 0),
+                          child: Container(
+                            width: 20,
+                            height: 20,
+                            decoration: BoxDecoration(
+                              color: Colors.blue,
+                              borderRadius: BorderRadius.circular(20),
+                            ),
+                          ),
+                        ),
+                        Transform.translate(
+                          offset: const Offset(-20, 0),
+                          child: Container(
+                            width: 20,
+                            height: 20,
+                            decoration: BoxDecoration(
+                              color: Colors.greenAccent,
+                              borderRadius: BorderRadius.circular(20),
+                            ),
+                            child: const Center(),
+                          ),
+                        ),
+                        Container(width: 5),
+                        const Text("Liked by aaaaaaaa and 116 others"),
+                        Container(
+                          height: 5,
                         )
                       ],
-                    );
-                  },
-                ),
-              )),
+                    )
+                  ],
+                );
+              },
+            ),
+          ),
         ],
       ),
       bottomNavigationBar: BottomNavigationBar(
@@ -125,5 +258,32 @@ class _MyHomePageState extends State<MyHomePage> {
         ],
       ),
     );
+  }
+
+  void ChangeFavouriteState() {
+    if (favouriteState) {
+      setState(() {
+        favouriteState = false;
+      });
+    } else {
+      setState(() {
+        favouriteState = true;
+      });
+    }
+  }
+
+  Icon FavouriteIcon() {
+    if (favouriteState) {
+      return const Icon(
+        Icons.favorite,
+        size: 25,
+        color: Colors.red,
+      );
+    } else {
+      return const Icon(
+        Icons.favorite_border,
+        size: 25,
+      );
+    }
   }
 }
